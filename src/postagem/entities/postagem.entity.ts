67e240883c50c14eb6771bd7323d5f21923903be
michel <michelc.desouza@gmail.com>
2024-04-18 20:35:25 -0300

    table relation

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,21 +1,27 @@
-import { IsNotEmpty } from "class-validator";
-import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { IsNotEmpty } from "class-validator"
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+import { Tema } from "../../tema/entities/tema.entity"
 
 @Entity({name: "tb_postagens"})
 export class Postagem {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+    @PrimaryGeneratedColumn()    
+    id: number
 
     @IsNotEmpty()
     @Column({length: 100, nullable: false})
-    titulo: string;
+    titulo: string
 
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
-    texto: string;
+    texto: string
 
     @UpdateDateColumn()
-    data: Date;
+    data: Date
+    
+    @ManyToOne(() => Tema, (tema) => tema.postagem, {
+        onDelete: "CASCADE"
+    })
+    tema: Tema
 
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -2,17 +2,22 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { DeleteResult, ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
+import { TemaService } from "src/tema/services/tema.service";
 
 @Injectable()
 export class PostagemService {
     constructor(
         @InjectRepository(Postagem)
-        private postagemRepository: Repository<Postagem>
+        private postagemRepository: Repository<Postagem>,
+        private temaService: TemaService
     ) { }
 
     async findAll(): Promise<Postagem[]> {
-        return await this.postagemRepository.find();
-
+        return await this.postagemRepository.find({
+            relations:{
+                tema: true
+            }
+        });
         // SELECT * FROM tb_postagens;
     }
 
@@ -21,6 +26,9 @@ export class PostagemService {
         let postagem = await this.postagemRepository.findOne({
             where: {
                 id
+            },
+            relations:{
+                tema: true
             }
         });
 
@@ -34,11 +42,25 @@ export class PostagemService {
         return await this.postagemRepository.find({
             where:{
                 titulo: ILike(`%${titulo}%`)
+            },
+            relations:{
+                tema: true
             }
         })
     }
 
     async create(postagem: Postagem): Promise<Postagem> {
+
+        if(postagem.tema){
+
+            let tema = await this.temaService.findById(postagem.tema.id)
+
+            if(!tema)
+                throw new HttpException('Tema não foi encontrado!', HttpStatus.NOT_FOUND)
+
+            return await this.postagemRepository.save(postagem);
+        }
+
         return await this.postagemRepository.save(postagem);
     }
 
@@ -48,6 +70,16 @@ export class PostagemService {
 
         if (!buscaPostagem || !postagem.id)
             throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+        if(postagem.tema){
+
+            let tema = await this.temaService.findById(postagem.tema.id)
+
+            if(!tema)
+                throw new HttpException('Tema não foi encontrado!', HttpStatus.NOT_FOUND)
+
+            return await this.postagemRepository.save(postagem);
+        }
 
         return await this.postagemRepository.save(postagem);
     }
