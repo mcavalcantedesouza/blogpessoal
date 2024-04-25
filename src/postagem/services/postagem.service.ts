45e91917ddb5ba0fd1e6c3d5988dd92ead810901
michel <michelc.desouza@gmail.com>
2024-04-25 10:48:18 -0300

    implantando a security

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -15,7 +15,8 @@ export class PostagemService {
     async findAll(): Promise<Postagem[]> {
         return await this.postagemRepository.find({
             relations:{
-                tema: true
+                tema: true,
+                usuario: true
             }
         });
         // SELECT * FROM tb_postagens;
@@ -28,7 +29,8 @@ export class PostagemService {
                 id
             },
             relations:{
-                tema: true
+                tema: true,
+                usuario: true
             }
         });
 
@@ -44,7 +46,8 @@ export class PostagemService {
                 titulo: ILike(`%${titulo}%`)
             },
             relations:{
-                tema: true
+                tema: true,
+                usuario: true
             }
         })
     }
